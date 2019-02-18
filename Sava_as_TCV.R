736623f6d9aba9ad6d662be8874931546bb615ff
RobDregmans <43481942+RobDregmans@users.noreply.github.com>
2019-02-18 16:58:48 +0100

    Add files via upload

---
+++ Sava_as_TCV.R
@@ -0,0 +1,19 @@
+#install.packages("rlist")
+library(rlist)
+
+df <- read.csv('BMMS_overview.csv',sep = ',', header = T)
+colnames(df)[which(names(df) == "ï..road")] <- "road"
+columns <- c("road", "LRPName", "lat", "lon")
+df <- df[columns]
+df[,3] <- as.numeric(df[,3])
+road_list <- unique(df$road)
+
+for (road in road_list) {
+  roaddata = list()
+  roaddata <- list.append(road)
+  df_t <- df[df$road == road, ]
+  for (i in 1:nrow(df_t)) {
+    roaddata <- list.append(roaddata, toString(df_t[i,2]), df_t[i,3], df_t[i,4])
+  }
+  write(roaddata, file = "data.tcv", ncolumns = length(roaddata), append = TRUE, sep = "\t")
+}
