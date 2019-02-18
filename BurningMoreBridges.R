6a7ca8105b1197cc8abb2d57667babb0d77ae9a2
lauracrowley97 <47539950+lauracrowley97@users.noreply.github.com>
2019-02-18 17:10:28 +0100

    Update and rename BurningBridges.R to BurningMoreBridges.R
    few updates of loops and importing csv of bridges  :))))

--- BurningMoreBridges.R
+++ BurningMoreBridges.R
@@ -1,43 +1,47 @@
-#this script
-#install.packages("geosphere") --> only avaailable for most recent version of R so check R.version up to date
+#this script requires that BMMMS_overview.xls is resaved as a .csv file in the same location within the same BMMS file in your working directory.
+#install.packages("geosphere")     --> only avaailable for most recent version of R so check R.version up to date
 library(biogeo)
+
+#think about adding two more cleaning:
+#   1. check unique of roadnames in bridge set against roadnames in road data set to find missing entire roads
+#   2. check unique of bridges (ie. same lrp number and some other field) to find duplicates in bridge data
 
 #chnage log documents the brigde id and the type of change
 change_log <- list('change_log')
 
-#Would prefere to restric these to static only - how to do in R?
+#Would prefere to restrict these to static only - how to do in R?
 #..NEED TO DEFINE THESE MORE INTUITIVELY
 #would be nice to run 'experiments' with these thresholds as controls
 threshold_match <- 20  #how close does a point have to be for an 'exact match' UNITS: metres
 threshold_chainage <- 10 #meters 'how close does the chainage difference have to be?'
 threshold_interpolate <- 200 #meters 'last resort: how close do 2 side by side points have to be fro interpolation?'
 
+#read in bridge data - making sure it has been saved as a .csv
+bridge_data <- read.csv('BMMS/BMMS_overview.csv', header = TRUE)
+bridge_data <- bridge_data[order(bridge_data$road),] #ordered by road number so it corresponds to our list of roads
 
-#function to convert co-ordinates to degrees decimsl (dd) for computations
-dms_dd <- function(degrees, minutes, seconds){
-  decimal_degrees <- degrees + minutes/60 + seconds/3600
-  return (decimal_degrees)
-}
 
+road_list <- read.csv(file = "_roadnames_list.csv", header = FALSE)
 #BIGGEST FOR LOOP HERE CYCLING THROUGH EACH ROAD
+i=0
+while(bridge_data$road == road_list[i]) { 
 
+for (row in 1:nrow(bridge_data)) { 
 #2nd BIGGEST FOR LOOP HERE CYCLING THROUGH EACH BRIDGE ON THE ROAD
 #variables THAT NEED TO BE IMPORTED FROM BMMS_Overview.csv TO BE WRITTEN !!!
-bridge_lrp         #this is the lrp name and not the structure id
-bridge_chainage   #in metres
-lat_bridge        #in degrees, minutes, seconds
-lon_bridge 
-estimated_loc
+bridge_lrp <- toString(bridge_data[row, "LRPName"])         #this is the lrp name and not the structure id
+bridge_chainage  <- bridge_data[row,"chainage"]    #in metres
+lat_bridge    <- bridge_data[row,"lat"]      #in degrees decimal
+lon_bridge   <- bridge_data[row,"lon"] 
+estimated_loc <-  toString(bridge_data[row, "EstimatedLoc"])
 #these all need to be rewritten after changes
 
-#convert co-ordinates to degrees decimal
-lat_bridge_dd <-  dms_dd ( lat_bridge[1] ,lat_bridge[2], lat_bridge[3] )
-lon_bridge_dd <-  dms_dd ( lon_bridge[1] ,lon_bridge[2], lon_bridge[3] )
 
 #to store the 2 closest lrp points found to possibly use for interpolation if no exact match
 closest_lrps <- matrix( nrow = 2,ncol = 2) 
 
 #3rd BIGGEST FOR LOOP HERE CYCLING THROUGH EACH LRP POINT ON THE ROAD
+for (lrp in road_data) {
 
 #variables that need to be imported from csv output of road data cleaning script
 lrp_roadpoint   #the id of the lrp on the road
@@ -45,10 +49,6 @@ road_chainge
 lat_roadpoint <- c(0,0,0)
 lon_roadpoint <- c(0,0,0)
 
-
-#coordinate conversion
-lat_roadpoint_dd <- dms_dd (lat_roadpoint[1], lat_roadpoint[2], lat_roadpoint[3])
-lon_roadpoint_dd <- dms_dd (lon_roadpoint[1], lon_roadpoint[2], lon_roadpoint[3])
 
 #calculate difference in chainage for the bridge and every lrp point on the road - could get a match!
 chainage_dif <- abs(lrp_chainage - road_chainage)
@@ -72,7 +72,7 @@ if (lrp_roadpoint == bridge_lrp){
     break ##is this the correct - or exit -- maybe need a flag and a loop :<
   }
   
-} else if (distm(c(lon_bridge_dd, lat_bridge_dd), c(lon_roadpoint_dd, lat_roadpoint_dd), fun = distHaversine) < threshold_match) 
+} else if (distm(c(lon_bridge, lat_bridge), c(lon_roadpoint, lat_roadpoint), fun = distHaversine) < threshold_match) 
   { #returns the distance in metres between 2 coordinates
   lat_bridge <- lat_roadpoint
   lon_bridge <- lon_roadpoint
@@ -107,7 +107,7 @@ if (lrp_roadpoint == bridge_lrp){
   }
 }
 
-
+}
 #END OF 3rd BIGGEST LOOP CYCLING THROUGH EACH LRP POINT ON THE ROAD
 
 
@@ -129,13 +129,18 @@ if (! NA %in% closest_lrps ) {
   change_log[length(change_log) +1] <- paste('Bridge number ', bridge_lrp,' no match found: ERROR/missing data.')
 }
 
-
+}
 #END OF 2nd BIGGEST LOOP CYCLING THROUGH EACH BRIDGE ON THE ROAD
 
-#must reupdate the details of the bridge to that big bridge data frame
 
 
+i <- i+ 1
+}
 #END OF BIGGEST LOOP HERE CYCLING THROUGH EACH ROAD
 
-#export change_log
+#export change_log --> could be prettier but who cares for now!
+write.csv(change_log,file ="change_log.csv")
+
+
 #export BMMS_Overview.csv
+write.csv(bridge_data, file = "BMMS/BMMS_Overview_Cleaned.csv")
