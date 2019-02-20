796734d542072499e43966a92d43d5a033a28fbf
martijnvanvlie <martijnvanvliet95@hotmail.com>
2019-02-20 15:01:24 +0100

    Update missing_roads.py

--- missing_roads.py
+++ missing_roads.py
@@ -11,13 +11,9 @@ cleaned_roads = pd.read_csv('cleaned_all_road_data kopie.csv', sep = '\t')
 missing_road_list = list(missing_road_list.columns.unique())
 missing_road = pd.DataFrame(missing_road_list, columns = ['Road'])
 cleaned_roads_missing = pd.merge(missing_road, cleaned_roads, on='Road', how='inner')
-#cleaned_roads_missing.Description = cleaned_roads_missing.Description.str.lower()
-
-df_bridge = cleaned_roads_missing[cleaned_roads_missing.LRPType == 'Bridge']
-df_culvert = cleaned_roads_missing[cleaned_roads_missing.LRPType == 'Culvert']
-df_bridge_culvert = #append , then sort on road , sort on LRPNo!!
-
-df_bridge[df_bridge.Description.str.contains('start') == True]
-df_bridge[df_bridge.Description.str.contains('end') == True]
-
+missing_bridge_culvert = cleaned_roads_missing[(cleaned_roads_missing.LRPType == 'Bridge') | (cleaned_roads_missing.LRPType == 'Culvert')]
+missing_bridge_culvert = missing_bridge_culvert.drop(missing_bridge_culvert[missing_bridge_culvert.Description.str.contains('end|End|END') == True].index)
+missing_bridge_culvert['Length'] = 'NaN'
+missing_bridge_culvert.drop('replaced', axis = 1)
+missing_bridge_culvert.to_csv("missing_bridge_culvert.csv", sep='\t')
 
