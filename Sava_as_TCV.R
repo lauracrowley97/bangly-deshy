dbd7607cfc845fd186fe53b01d168eb60985b9d7
Emma <emma.castanos@gmail.com>
2019-02-20 16:33:29 +0100

    someupdates

--- Sava_as_TCV.R
+++ Sava_as_TCV.R
@@ -3,20 +3,21 @@ library(rlist)
 
 
 
-df <- read.csv('cleaned_all_road_data.csv',sep = '\t', header = T)
-df$LRPNo <- toString(df['LRPNo'])
-colnames(df)[which(names(df) == "ï..road")] <- "road"
-columns <- c("road", "LRPName", "lat", "lon") #update this
+df <- read.csv('cleaned_all_road_data.csv',sep = '\t', header = T, stringsAsFactors = FALSE)
+library('rlist')
+#colnames(df)[which(names(df) == "ï..road")] <- "road"
+columns <- c("Road", "LRPNo", "LatitudeDec", "LongitudeDec") #update this
 df <- df[columns]
-df[,3] <- as.numeric(df[,3])
-road_list <- unique(df$road)
+#df[,3] <- as.numeric(df[,3])
+road_list <- unique(df$Road)
 
 for (road in road_list) {
+  #road = "N1"
   roaddata = list()
   roaddata <- list.append(road)
-  df_t <- df[df$road == road, ]
+  df_t <- df[df$Road == road, ]
   for (i in 1:nrow(df_t)) {
-    roaddata <- list.append(roaddata, toString(df_t[i,2]), df_t[i,3], df_t[i,4])
+    roaddata <- list.append(roaddata, df_t[i,2], df_t[i,3], df_t[i,4])
   }
   write(roaddata, file = "data.tcv", ncolumns = length(roaddata), append = TRUE, sep = "\t")
 }
