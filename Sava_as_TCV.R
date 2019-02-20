d2f2455bdc51ed81896d712f8adea889de17bace
Emma <emma.castanos@gmail.com>
2019-02-20 23:49:53 +0100

    fix file read in for report

--- Sava_as_TCV.R
+++ Sava_as_TCV.R
@@ -4,7 +4,7 @@ library(rlist)
 #library(tidyr)
 
 
-df <- read.csv('cleaned_all_road_data_v2_IVAR.csv',sep = '\t', header = T, stringsAsFactors = FALSE)
+df <- read.csv('cleaned_all_road_data_v2.csv',sep = '\t', header = T, stringsAsFactors = FALSE)
 library('rlist')
 #colnames(df)[which(names(df) == "ï..road")] <- "road"
 columns <- c("Road", "LRPNo", "LatitudeDec", "LongitudeDec") #update this
