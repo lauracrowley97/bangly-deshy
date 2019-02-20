79b9ab76ae1f2d1c78e4317d604fab89bd009edf
Emma <emma.castanos@gmail.com>
2019-02-20 21:01:14 +0100

    update tcv conversion

--- Sava_as_TCV.R
+++ Sava_as_TCV.R
@@ -1,6 +1,7 @@
 #install.packages("rlist")
+#install.packages("tidyr")
 library(rlist)
-
+#library(tidyr)
 
 
 df <- read.csv('cleaned_all_road_data_2.csv',sep = '\t', header = T, stringsAsFactors = FALSE)
@@ -11,11 +12,18 @@ df <- df[columns]
 #df[,3] <- as.numeric(df[,3])
 road_list <- unique(df$Road)
 
+#spread(df, key = numbers, value = value)
+
+#m1 <- t(df)
+#d2 <- data.frame(r1=row.names(m1), m1, row.names = NULL)
+
+
 for (road in road_list) {
   #road = "N1"
   roaddata = list()
   roaddata <- list.append(road)
   df_t <- df[df$Road == road, ]
+  df_t <- df_t[order(df_t[,'Road']), ]
   for (i in 1:nrow(df_t)) {
     roaddata <- list.append(roaddata, df_t[i,2], df_t[i,3], df_t[i,4])
   }
